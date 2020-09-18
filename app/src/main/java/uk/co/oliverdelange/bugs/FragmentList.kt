eddb028922e06f22f01372567ad097eacee47b39
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2020-09-18 18:00:30 +0100

    Added example of scrollToPosition not working

--- app/src/main/java/uk/co/oliverdelange/bugs/FragmentList.kt
+++ app/src/main/java/uk/co/oliverdelange/bugs/FragmentList.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import com.xwray.groupie.kotlinandroidextensions.Item
@@ -16,11 +17,12 @@ import kotlinx.android.synthetic.main.fragment_list.*
 import kotlinx.android.synthetic.main.item.view.*
 
 class FragmentList : Fragment() {
+    val manager by lazy { LinearLayoutManager(requireContext()) }
     val adapter = GroupAdapter<GroupieViewHolder>()
-    val items = (1..100).map {
-        MyItem(it) {
+    val items = (1..100).map { i ->
+        MyItem(i) {
             swiperefresh.isRefreshing = false
-            Toast.makeText(context, "Clicked!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Clicked $i!", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -33,16 +35,19 @@ class FragmentList : Fragment() {
             Log.v("TEST", "Pull to refresh")
         }
 
+        recycler.layoutManager = manager
         recycler.adapter = adapter
 
         adapter.update(items)
+
+        recycler.scrollToPosition(50) // Works, but not with ViewPager2
     }
 }
 
 class MyItem(val i: Int, val onClick: () -> Unit) : Item() {
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         Log.w("TEST", "Binding $i $position")
-        viewHolder.itemView.textView.text = position.toString()
+        viewHolder.itemView.textView.text = i.toString()
         viewHolder.itemView.setOnClickListener {
             Log.w("TEST", "Clicked $i $position")
             onClick()
