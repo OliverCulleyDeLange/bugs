a59b05cce4b5a7504260505d2a2fc8db07b3c410
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2020-09-21 15:09:13 +0100

    View bug with nesting NumberPicker inside afollestad material dialog with custom view

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 Bug details:
 
-The Main activity is horizontally split. The top section exhibits the bug, the bottom section does not. 
-The only difference between the top and bottom section is that the top section is wrapped in a viewpager2, and the bottom section is not. 
-
-The top section does not invoke the recycler view items click handlers when tapped, unless the recycler view has been scrolled down a bit. 
-
-The click handlers are invoked if the SwipeRefreshLayout layout_height is set to match_parent, but i need the SwipeRefreshLayout to match constraints. 
+NumberPicker inside afollestad material dialog gets 1/3 cut off the bottom when viewing on One Plus 5T, android 9, oxygen os 9.0.10
--- app/src/main/java/uk/co/oliverdelange/bugs/MainActivity.kt
+++ app/src/main/java/uk/co/oliverdelange/bugs/MainActivity.kt
@@ -1,28 +1,69 @@
 package uk.co.oliverdelange.bugs
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.NumberPicker
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.fragment.app.DialogFragment
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.customview.getCustomView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.picker1.view.*
 
 class MainActivity : AppCompatActivity() {
 
+    var d: MaterialDialog? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        thisWorks.setupIntRange(10..100, 50)
+        alsoWorks.picker1_1.setupIntRange(10..100, 50)
     }
 
     override fun onStart() {
         super.onStart()
-        pager.adapter = object : FragmentStateAdapter(this) {
-            override fun getItemCount(): Int = 3
-            override fun createFragment(position: Int): Fragment = FragmentList()
+        buttonb.setOnClickListener {
+            NormalDialog().show(supportFragmentManager, "TAG")
         }
+        buttona.setOnClickListener {
+            d = MaterialDialog(this).show {
+                title(text = "Cut off picker")
+                picker1_i(prefill = 50, values = 10..100)
+            }
+        }
+    }
+}
 
-        TabLayoutMediator(tabs, pager) { tab, position ->
-            tab.text = position.toString()
-        }.attach()
+fun MaterialDialog.picker1_i(
+    prefill: Int = 0,
+    values: IntRange = 1..10
+): MaterialDialog {
+    customView(R.layout.picker1, scrollable = true)
+    getCustomView().apply {
+        picker1_1.setupIntRange(1..5, 3)
+    }
+    return this
+}
+
+fun NumberPicker.setupIntRange(values: IntRange, prefillIndex: Int) {
+    minValue = values.first
+    maxValue = values.last
+    value = prefillIndex
+}
+
+class NormalDialog : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val view = requireActivity().layoutInflater.inflate(R.layout.picker1, null)
+            view.picker1_1.setupIntRange(1..100, 50)
+            // Use the Builder class for convenient dialog construction
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("Test")
+            builder.setView(view)
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
     }
 }
